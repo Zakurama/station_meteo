1e550cdca55e401c47ed3acef54e4afa457a68db
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2025-01-31 11:53:15 +0100

    code esp32

--- station_meteo.ino
+++ station_meteo.ino
@@ -2,66 +2,57 @@
 #include <WiFiClientSecure.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>          
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+#include <Arduino.h>
 #include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BMP280.h>
+#include <ArtronShop_BH1750.h>
+#include <ArduinoJson.h>
+#include "Adafruit_Si7021.h"
 
 // Utile pour les mots de passe
 #include "config.h"
 
-// Déclaration de la broche d'entrée du capteur de temperature
-#define KY001_Signal_PIN 5
-// Configuration des libraries
-OneWire oneWire(KY001_Signal_PIN);          
-DallasTemperature sensors(&oneWire);   
-
-// Définir les broches du BMP280
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11 
-#define BMP_CS 10
-// Initialiser BMP280
-Adafruit_BMP280 bmp;
-
-// Déclaration de la broche d'entrée du capteur d'humidité
-#define DHTPIN 2     
-// Le capteur est initialisé
-#define DHTTYPE DHT11   // DHT 11
-DHT dht(DHTPIN, DHTTYPE);
+#define FREQ_MESURE 5000 // ms
+#define WIFI_DELAY 500 // ms
+#define NB_MESURES_STOCKEE 100
 
 // Connexion Wifi
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
+const char* ssid_wifi = WIFI_SSID;
+const char* password_wifi = WIFI_PASSWORD;
 
-// Certificat serveur
+// Information serveur
 const char* rootCACertificate = CERTIFICATE;
+const char* url_serveur = SERVER_URL;
+const int serveur_port = SERVER_PORT;
+const char* server_password = SERVER_PASSWORD;  
+const char* post_endpoint = POST_ENDPOINT;
 
+// Création des différents objets
 WiFiClientSecure client;
+Adafruit_Si7021 sensor = Adafruit_Si7021();
+ArtronShop_BH1750 bh1750(0x23, &Wire); 
+
+// Création du format d'envoi JSON 
+const size_t CAPACITY = JSON_OBJECT_SIZE(2) + JSON_ARRAY_SIZE(NB_MESURES_STOCKEE) + NB_MESURES_STOCKEE * JSON_OBJECT_SIZE(4);  
+// 2 car deux champs dans le JSON (password et data) et 4 parce data a 4 champs (température, luminosite, humidite, timestamp)
+DynamicJsonDocument doc(CAPACITY);
+// Tableau de stokage des valeurs (si non connectée au serveur)
+JsonArray data = doc.createNestedArray("data");
 
 void setup()
 {
     Serial.begin(115200);
-    
-    // Initialisation des capteurs
-    
-    sensors.begin();
-    dht.begin();
-    delay(10);
-    
+        
     // Connexion Wifi
 
     Serial.println();
     Serial.println();
     Serial.print("Connecting to ");
-    Serial.println(ssid);
+    Serial.println(ssid_wifi);
 
-    WiFi.begin(ssid, password);
+    WiFi.begin(ssid_wifi, password_wifi);
 
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
+        delay(WIFI_DELAY);
         Serial.print(".");
     }
 
@@ -71,9 +62,10 @@ void setup()
     Serial.println(WiFi.localIP());
 
     // Connexion au serveur
+    
     client.setCACert(rootCACertificate);
 
-    if (!client.connect("meteo-iot.rezoleo.fr", 443)) {
+    if (!client.connect(url_serveur, serveur_port)) {
         Serial.println("Échec de connexion au serveur !");
         return;
     }
@@ -81,34 +73,73 @@ void setup()
       Serial.println("Connexion au serveur réussie !");
       }
     
-    //Setup capteur pression
-    if (!bmp.begin()) {  
-      Serial.println(F("N'a pas trouvé de capteur BMP280 valide, vérifier le câblage!"));
-      while (1);
+    //Setup des capteurs
+    Wire.begin();
+    if (!bh1750.begin()) {
+      Serial.println("BH1750 non trouvé");
+      while(true);
     }
+  
+    if (!sensor.begin()) {
+      Serial.println("Si7021 non trouvé");
+      while (true);
+    }
+
+    Serial.println("Capteurs bh1750 et Si7021 trouvés");
+
+    // Ajout du mot de passe au JSON
+    doc["password"] = server_password;
 }
 
 void loop() {
-  
-    // On récupère les valeurs des capteurs
-    sensors.requestTemperatures(); 
-    float temperature = sensors.getTempCByIndex(0);
-    float humidity = dht.readHumidity();
-    float pressure = bmp.readPressure()/100;
+
+     // On récupère les valeurs des capteurs
+     float luminosite = bh1750.light(); // En lux 
+     float humidite = sensor.readHumidity();// En % ?
+     float temperature = sensor.readTemperature();// En C°
+        
+     // Création d'une nouvelle mesure
+     JsonObject measurement = data.createNestedObject();
+     measurement["temperature"] = temperature;
+     measurement["luminosity"] = luminosite;
+     measurement["humidite"] = humidite;
+     measurement["timestamp"] = millis();
     
-    // On envoie les infos au serveur
-    String password = SERVER_PASSWORD;  
-    String jsonPayload = "{\"temperature\": " + String(temperature) + ", \"pression\": "+ String(pressure) +", \"humidite\": "+ String(humidity) +", \"password\":\""+ password +"\"}";
+     if (WiFi.status() != WL_CONNECTED){
+         // On essaie de se reconnecter
+         Serial.println("Station non connectée au Wifi ! Reconnexion en cours...");
+         WiFi.disconnect();
+         WiFi.reconnect();
+     }
+     else if (!client.connected()) {
+         Serial.println("Station non connectée au serveur, tentative de reconnexion...");
+         client.stop();  // Fermer la connexion précédente
+         if (client.connect(url_serveur, serveur_port)) {
+             Serial.println("Reconnecté au serveur");
+          } else {
+             Serial.println("Échec de la reconnexion");
+          }
+      }
 
-    // Envoyer une requête POST
-    client.println("POST https://meteo-iot.rezoleo.fr/recup_info_meteo.php HTTP/1.1");
-    client.println("Host: meteo-iot.rezoleo.fr");
-    client.println("Content-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(jsonPayload.length());
-    client.println();
-    client.println(jsonPayload);
-    Serial.println("Payload envoyée : " + jsonPayload);
+     else{
 
-    delay(10000);  // Attendre avant de renvoyer
+        // Conversion JSON en String
+        String jsonPayload;
+        serializeJson(doc, jsonPayload);
+        
+        // Envoyer une requête POST
+        client.println("POST "+ String(post_endpoint)+" HTTP/1.1"); 
+        client.println("Host: " + String(url_serveur));
+        client.println("Content-Type: application/json");
+        client.print("Content-Length: ");
+        client.println(jsonPayload.length());
+        client.println();
+        client.println(jsonPayload);
+        Serial.println("Payload envoyée : " + jsonPayload);
+
+        // Réinitialisation du buffer
+        data.clear();
+    }
+    
+    delay(FREQ_MESURE);  // Attendre avant de renvoyer
 }
